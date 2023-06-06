8e29302d67ead922a166b12d66cee6a66d3833ac
Drako Vishious <89056471+Drknyan@users.noreply.github.com>
2023-06-05 20:29:42 -0500

    Add files via upload

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+# CHANGELOG
+
+## 0.01
+
+- Add support for Foundry v10
---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const project-yggdrasil = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ project-yggdrasil.abilities = {
+  "str": "project-yggdrasil.AbilityStr",
+  "dex": "project-yggdrasil.AbilityDex",
+  "con": "project-yggdrasil.AbilityCon",
+  "int": "project-yggdrasil.AbilityInt",
+  "wis": "project-yggdrasil.AbilityWis",
+  "cha": "project-yggdrasil.AbilityCha"
+};
+
+project-yggdrasil.abilityAbbreviations = {
+  "str": "project-yggdrasil.AbilityStrAbbr",
+  "dex": "project-yggdrasil.AbilityDexAbbr",
+  "con": "project-yggdrasil.AbilityConAbbr",
+  "int": "project-yggdrasil.AbilityIntAbbr",
+  "wis": "project-yggdrasil.AbilityWisAbbr",
+  "cha": "project-yggdrasil.AbilityChaAbbr"
+};
